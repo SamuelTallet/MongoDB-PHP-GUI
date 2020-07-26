cf269dc2c872cb4a0bf9be0eb24b8f30eee4ed3c
Samuel Tallet <samuel.tallet@gmail.com>
2020-07-26 10:07:13 +0200

    Fix SQL conversion issues

--- src/Controllers/SQLController.php
+++ src/Controllers/SQLController.php
@@ -15,13 +15,17 @@ class SQLController extends Controller {
             return new JsonResponse(400, ErrorNormalizer::normalize($th, __METHOD__));
         }
 
+        // Converter takes only inline SQL as input.
+        $decodedRequestBody['sql'] = str_replace("\n", ' ', $decodedRequestBody['sql']);
+
+        $decodedRequestBody['sql'] = str_replace('"', '\"', $decodedRequestBody['sql']);
+
         $jarPath = '"' . MPG_ABS_PATH 
             . '/static/jar/sql-to-mongo-db-query-converter-1.13-standalone.jar"';
-        $jarArgs = '--sql "' . str_replace('"', '\"', $decodedRequestBody['sql']) . '"';
+        $jarArgs = '--sql "' . $decodedRequestBody['sql'] . '"';
         
         $command = 'java -jar ' . $jarPath . ' ' . $jarArgs;
 
-        // If OS is Windows:
         if ( strtoupper(substr(PHP_OS, 0, 3)) === 'WIN' ) {
             // Force output.
             $command .= ' 2>&1';
@@ -32,7 +36,7 @@ class SQLController extends Controller {
         $mongoDBQuery = [];
 
         try {
-            if ( !preg_match_all('|^.*\.find\((.*)\)$|s', $commandResult, $mongoDBQuery) ) {
+            if ( !preg_match_all('|^.*\.find\({(.*)}\)$|s', $commandResult, $mongoDBQuery) ) {
                 throw new \Exception(
                     'Impossible to convert (SELECT) SQL query to MongoDB query... ' . 
                     'Try to install Java JDK on the computer hosting "' . MPG_APP_NAME . '".'
@@ -42,7 +46,7 @@ class SQLController extends Controller {
             return new JsonResponse(500, ErrorNormalizer::normalize($th, __METHOD__));
         }
 
-        return new JsonResponse(200, $mongoDBQuery[1][0]);
+        return new JsonResponse(200, '{' . $mongoDBQuery[1][0] . '}');
 
     }
     
