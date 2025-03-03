9dec6f800cf3e163b7b6306e0600e9ebbfe49c50
Samuel Tallet <36248671+SamuelTallet@users.noreply.github.com>
2025-03-03 14:21:11 +0100

    Bump version to 1.3.2

--- index.php
+++ index.php
@@ -6,7 +6,7 @@ use Limber\Application;
 use Capsule\Factory\ServerRequestFactory;
 use Limber\Exceptions\NotFoundHttpException;
 
-const VERSION = '1.3.1';
+const VERSION = '1.3.2';
 
 /**
  * Absolute path, without trailing slash.
